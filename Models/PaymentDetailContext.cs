92c636a80b9ad44da73f97e9bafbc67390c05a73
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2025-09-17 16:26:53 +0100

    Add project files.

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+
+namespace PaymentAPI.Models
+{
+	public class PaymentDetailContext : DbContext
+	{
+		public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+		{
+
+		}
+		public DbSet<PaymentDetail> PaymentDetails { get; set; }
+	}
+}
---
+++ PaymentApp/src/app/payment-details/payment-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-details',
+  imports: [],
+  templateUrl: './payment-details.html',
+  styles: ``
+})
+export class PaymentDetails {
+
+}
